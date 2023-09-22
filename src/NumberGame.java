abf293b0926b65e8358e82c8e5c948c10ffe0a94
Janhavi <janhavidahatonde@gmail.com>
2023-09-22 10:06:38 +0530

    Task 1

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -2,15 +2,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGame {
+    //    Initializing the score
     static int score = 0;
+    //    Initializing user input
     static int userInput;
+    //    Intializing exit control variable
     static String exit;
 
     public static void main(String[] args) {
+//        Creating an object of Random class
         Random random = new Random();
+//        Creating an object for Scanner class
         Scanner sc = new Scanner(System.in);
         do {
+//            Declaring the range of random number (1 to 100) and generating it
             int randNum = (random.nextInt(100 - 1 + 1) + 1);
+//            Initializing the number of attempts
             int attempts = 5;
             do {
                 System.out.print("Guess the number: ");
